949a9dd15c1ebadb8bdb18d8473f7d196948c885
Vanshul Kumar (kumarv26) <bhadanavanshul@gmail.com>
2023-04-09 15:17:35 +0100

    disclaimer-text-component-created
    New component with disclaimer text is created and added to the results model.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -38,10 +38,6 @@ export default function Home() {
         <div className={styles.grid}>{courseTemplates}</div>
       </main>
 
-      <footer className={styles.footer}>
-        <p>Disclaimer! This calculator has not been officially verified as accurate against Coventry University&apos;s internal systems. However, it does follow the algorithms correctly (to the best of our knowledge).</p>
-        <p>by Armandas Barkauskas</p>
-      </footer>
     </div>
   );
 }
